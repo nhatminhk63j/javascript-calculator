5e4574b0dcc6365a47263fd2e516519cbd0b1375
Nhat Ngo Sach <nhatngo11a1@gmail.com>
2020-02-04 10:21:15 +0700

    finished project

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import Calculator from './Calculator'
 
-ReactDOM.render(<div>Init</div>, document.getElementById('root'));
+ReactDOM.render( < Calculator / > , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
